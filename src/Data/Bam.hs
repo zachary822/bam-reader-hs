faf5ee40ff7ad4c7a43d6dd34c50c4fc8002d47f
Zachary Juang <zachary822@me.com>
2024-03-11 04:40:57 -0400

    simplify

--- src/Data/Bam.hs
+++ src/Data/Bam.hs
@@ -14,45 +14,35 @@ import Data.ByteString (ByteString)
 import Data.ByteString qualified as B
 import Data.ByteString.Lazy qualified as BL
 import Data.Digest.CRC32
-import Data.Int
 import Data.Void
 import Data.Word
+import Debug.Trace
 import Text.Megaparsec
 import Text.Megaparsec.Byte
+import Text.Megaparsec.Byte.Binary
 
 type Parser = Parsec Void ByteString
 
-pWord16le :: Maybe String -> Parser Word16
-pWord16le n = runGet (getWord16le) . BL.fromStrict <$> takeP n 2
-
-pInt16le :: Maybe String -> Parser Int16
-pInt16le n = runGet (getInt16le) . BL.fromStrict <$> takeP n 2
-
-pWord32le :: Maybe String -> Parser Word32
-pWord32le n = runGet getWord32le . BL.fromStrict <$> takeP n 4
-
-pInt32le :: Maybe String -> Parser Int32
-pInt32le n = runGet getInt32le . BL.fromStrict <$> takeP n 4
-
-pFloatle :: Maybe String -> Parser Float
-pFloatle n = runGet getFloatle . BL.fromStrict <$> takeP n 4
+floatle :: Parser Float
+floatle = do
+  runGet (getFloatle) . BL.fromStrict <$> takeP (Just "32-bit floating point") 4
 
 parseBzgfBlock :: Parser ByteString
 parseBzgfBlock = do
   _ <- string "\x1f\x8b\x08\04" -- bzgf magic
   _ <- takeP Nothing 6
 
-  xlen <- fromIntegral <$> pWord16le (Just "XLEN")
+  xlen <- fromIntegral <$> word16le
   unless (xlen == 6) (fail "xlen not 6")
 
   _ <- string "BC\x02\x00"
 
-  bsize <- pWord16le (Just "BSIZE")
+  bsize <- word16le
   deflate <-
     BL.toStrict . decompress . BL.fromStrict
       <$> takeP (Just "CDATA") (fromIntegral bsize - xlen - 19)
 
-  crc <- pWord32le (Just "CRC32")
+  crc <- word32le
   unless (crc == crc32 deflate) (fail "CRC32 checksum mismatch")
 
   _ <- takeP Nothing 4
@@ -64,9 +54,11 @@ parseBzgf = B.concat <$> many parseBzgfBlock <* eof
 
 pRef :: Parser Ref
 pRef = do
-  l_name <- pWord32le Nothing
+  l_name <- word32le
   name <- takeP (Just "ref_name") (fromIntegral l_name)
-  l_ref <- pWord32le (Just "l_ref")
+  l_ref <- word32le
+
+  traceShowM l_ref
 
   return (name, fromIntegral l_ref)
 
@@ -84,15 +76,15 @@ pTag = do
     -- C
     67 -> Word8Value . fromIntegral <$> anySingle
     -- s
-    115 -> Int16Value <$> pInt16le Nothing
+    115 -> Int16Value <$> int16le
     -- S
-    83 -> Word16Value <$> pWord16le Nothing
+    83 -> Word16Value <$> word16le
     -- i
-    105 -> Int32Value <$> pInt32le Nothing
+    105 -> Int32Value <$> int32le
     -- I
-    73 -> Word32Value <$> pWord32le Nothing
+    73 -> Word32Value <$> word32le
     -- f
-    102 -> FloatValue <$> pFloatle Nothing
+    102 -> FloatValue <$> floatle
     -- Z
     90 -> BSValue <$> takeWhileP Nothing (/= 0) <* char 0
     -- H
@@ -100,15 +92,16 @@ pTag = do
     -- B
     66 -> do
       t <- anySingle
-      cnt <- fromIntegral <$> pWord32le Nothing
+
+      cnt <- fromIntegral <$> word32le
       case t of
         99 -> Int8ListValue . fmap fromIntegral . B.unpack <$> takeP Nothing cnt
         67 -> Word8ListValue . fmap fromIntegral . B.unpack <$> takeP Nothing cnt
-        115 -> Int16ListValue <$> count cnt (pInt16le Nothing)
-        83 -> Word16ListValue <$> count cnt (pWord16le Nothing)
-        105 -> Int32ListValue <$> count cnt (pInt32le Nothing)
-        73 -> Word32ListValue <$> count cnt (pWord32le Nothing)
-        102 -> FloatListValue <$> count cnt (pFloatle Nothing)
+        115 -> Int16ListValue <$> count cnt int16le
+        83 -> Word16ListValue <$> count cnt word16le
+        105 -> Int32ListValue <$> count cnt int32le
+        73 -> Word32ListValue <$> count cnt word32le
+        102 -> FloatListValue <$> count cnt floatle
         _ -> fail "bad value type"
     _ -> fail "bad value type"
 
@@ -121,27 +114,27 @@ pTags end = do
 
 pBlock :: Refs -> Parser ByteString
 pBlock refs = do
-  blockSize <- pWord32le (Just "block_size")
+  blockSize <- word32le
 
-  ref <- (refs !!) . fromIntegral <$> pInt32le (Just "refID")
+  ref <- (refs !!) . fromIntegral <$> int32le
 
-  pos <- pInt32le (Just "pos")
+  pos <- int32le
 
   l_read_name :: Word8 <- anySingle <?> "l_read_name"
 
   mapq :: Word8 <- anySingle <?> "mapq"
 
-  bin <- pWord16le (Just "bin")
+  bin <- word16le
 
-  n_cigar_op <- pWord16le (Just "n_cigar_op")
-  flag <- pWord16le (Just "flag")
-  l_seq <- pWord32le (Just "l_seq")
-  next_refID <- pInt32le (Just "next_refID")
-  next_pos <- pInt32le (Just "next_pos")
-  tlen <- pInt32le (Just "tlen")
+  n_cigar_op <- word16le
+  flag <- word16le
+  l_seq <- word32le
+  next_refID <- int32le
+  next_pos <- int32le
+  tlen <- int32le
   read_name <- takeP (Just "read_name") (fromIntegral l_read_name)
   cigar <-
-    cigarToPrintable <$> count (fromIntegral n_cigar_op) (pWord32le (Just "cigar"))
+    cigarToPrintable <$> count (fromIntegral n_cigar_op) word32le
   code <-
     (`bin2code` l_seq) <$> takeP (Just "seq") ((fromIntegral l_seq + 1) `div` 2)
   qual <- qualToPrintable <$> takeP (Just "qual") (fromIntegral l_seq)
@@ -165,11 +158,11 @@ pBlock refs = do
 parseBam :: Parser [ByteString]
 parseBam = do
   _ <- string "BAM\x01"
-  l_text <- pWord32le (Just "l_text")
+  l_text <- word32le
 
   header <- takeP (Just "text") (fromIntegral l_text)
 
-  n_ref <- pWord32le (Just "n_ref")
+  n_ref <- word32le
 
   refs <- count (fromIntegral n_ref) pRef
 
